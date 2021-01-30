e077f0b0706d409c51ba16dd6d97ed7c87f65c0c
Milena <milenramiro@gmail.com>
2021-01-30 19:49:20 -0300

    Initial Commit

---
+++ API_Pessoas/Controllers/PessoasController.cs
@@ -0,0 +1,58 @@
+﻿using API_Pessoas.Model;
+using API_Pessoas.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API_Pessoas.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PessoasController : ControllerBase
+    {
+        private IPessoaService _pessoa;
+
+        private readonly ILogger<PessoasController> _logger;
+
+        public PessoasController(ILogger<PessoasController> logger, IPessoaService pessoa)
+        {
+            _logger = logger;
+            _pessoa = pessoa;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_pessoa.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _pessoa.FindByID(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] tbPessoa pessoa)
+        {
+            if (pessoa == null) return BadRequest();
+            return Ok(_pessoa.Create(pessoa));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] tbPessoa pessoa)
+        {
+            if (pessoa == null) return BadRequest();
+            return Ok(_pessoa.Update(pessoa));
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(long id)
+        {
+            _pessoa.Delete(id);
+            return NoContent();
+        }
+
+    }
+}
---
+++ API_Pessoas/Services/Implementations/PessoaServiceImplementation.cs
@@ -0,0 +1,36 @@
+﻿using API_Pessoas.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Pessoas.Services.Implementations
+{
+    public class PessoaServiceImplementation : IPessoaService
+    {
+        public tbPessoa Create(tbPessoa person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<tbPessoa> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public tbPessoa FindByID(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public tbPessoa Update(tbPessoa person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
