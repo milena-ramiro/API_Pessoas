4496b8209c6e00ad78a568c04fe67ad7a213da9f
Milena <milenramiro@gmail.com>
2021-01-30 19:59:56 -0300

    Adicionei DataContext e a string de conexao

---
+++ API_Pessoas/Model/Context/MySqlContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_Pessoas.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext() { }
+
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
+
+        public DbSet<tbPessoa> Pessoa { get; set; }
+    }
+}
